20f7da17c5d25ffc0e5250334f6147f1422fcdf7
zpotts-cnu <zachary.potts.19@cnu.edu>
2022-11-17 22:33:49 -0500

    Updated blink.c example

--- blink/blink.c
+++ blink/blink.c
@@ -1,22 +1,14 @@
 #include "pico/stdlib.h"
-//#include "pico/cyw43_arch.h"
 
 int main() {
-    // stdio_init_all();
-    // if (cyw43_arch_init()) {
-    //     printf("WiFi init failed");
-    //     return -1;
-    // }
     const uint LED_PIN = 22;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, true);
-        // cyw43_arch_gpio_put();
-        sleep_ms(330);
+        sleep_ms(1000);
         gpio_put(LED_PIN, false);
-        // cyw43_arch_gpio_put();
-        sleep_ms(330);
+        sleep_ms(1000);
     }
     return 0;
 }
