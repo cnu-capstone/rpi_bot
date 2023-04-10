c360a4657298fc96a3d9c70ee7bca8ca0a87e31d
zpotts-cnu <zachary.potts.19@cnu.edu>
2023-04-10 12:07:53 -0400

    demo bash script

--- demo.sh
+++ demo.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+trap "sudo pkill uart_comm" SIGINT
+
 while :
 do
 cd /home/pi/Desktop/AWS/
 python awsNotification.py
 sleep 1
 cd /home/pi/Documents/rpi_bot/
-./pi4/uart_comm/uart_comm
-PID=$!
-sleep 1
-kill -INT $PID
+./pi4/uart_comm/uart_comm &
+sleep 5
+sudo pkill uart_comm
 done
