19f5e36b8d693b65a4fecf9c5923adf5dc1ec076
zpotts-cnu <zachary.potts.19@cnu.edu>
2023-04-15 16:51:47 -0400

    Updated ultrasonic and motor_controller usage to linking header files

---
+++ includes/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#ifndef SUCCESS
+#define SUCCESS 0
+#endif
+
+#endif
---
+++ includes/ultrasonic.h
@@ -0,0 +1,38 @@
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+
+#include "constants.h"
+
+#define TRIGGER_PIN_LEFT 2  // GPIO pin number for trigger
+#define ECHO_PIN_LEFT 3   // GPIO pin number for echo
+
+#define TRIGGER_PIN_RIGHT 0  // GPIO pin number for trigger
+#define ECHO_PIN_RIGHT 1    // GPIO pin number for echo
+
+#define TRIGGER_PIN_FORWARD_A 4  // GPIO pin number for trigger
+#define ECHO_PIN_FORWARD_A 5   // GPIO pin number for echo
+#define TRIGGER_PIN_FORWARD_B 6  // GPIO pin number for trigger
+#define ECHO_PIN_FORWARD_B 7   // GPIO pin number for echo
+
+void echo_isr_LEFT(uint gpio, uint32_t events);
+
+void echo_isr_RIGHT(uint gpio, uint32_t events);
+
+void echo_isr_FORWARD_A(uint gpio, uint32_t events);
+
+void echo_isr_FORWARD_B(uint gpio, uint32_t events);
+
+float ultrasonic_left();
+
+float ultrasonic_right();
+
+float ultrasonic_forward_a();
+
+float ultrasonic_forward_b();
+
+#endif
