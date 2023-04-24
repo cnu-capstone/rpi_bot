8333ac6f6e3ed784c1a419e269139907f0b33575
zpotts-cnu <zachary.potts.19@cnu.edu>
2023-04-24 01:27:59 -0400

    Updated demo.sh for new routing script

--- demo.sh
+++ demo.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-trap "sudo pkill uart_comm" SIGINT
+# trap "sudo pkill uart_comm" SIGINT
+# trap "sudo pkill rpi_routing" SIGINT
+trap "sudo pkill demo.sh" SIGINT
 
 while :
 do
 cd /home/pi/Desktop/AWS/
 python awsNotification.py
 sleep 1
-cd /home/pi/Documents/rpi_bot/
-./pi4/uart_comm/uart_comm &
+# cd /home/pi/Documents/rpi_bot/
+# ./pi4/uart_comm/uart_comm &
+cd /home/pi/Documents/rpi_routing/
+./Release/rpi_routing &
 sleep 5
-sudo pkill uart_comm
+# sudo pkill uart_comm
+sudo pkill rpi_routing
 done
